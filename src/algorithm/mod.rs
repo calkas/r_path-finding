4f5c488a638fed6e48b896f34f6c335fe82a12ce
Piotr Rybka <piotr.rybka@nokia.com>
2025-04-30 13:06:57 +0200

    create pathfinder

--- src/algorithm/mod.rs
+++ src/algorithm/mod.rs
@@ -1,4 +1,5 @@
 use crate::map::{grid::Grid, TitleCoords};
+use std::collections::HashMap;
 pub mod bfs;
 pub mod dijkstra;
 
@@ -65,5 +66,59 @@ impl SimulationCoordinator {
         }
         self.accumulated_time = 0.0;
         true
+    }
+}
+
+#[derive(Default)]
+pub struct Pathfinder {
+    path: HashMap<TitleCoords, Option<TitleCoords>>,
+    solution: Vec<TitleCoords>,
+}
+
+impl Pathfinder {
+    pub fn create_path(&mut self, came_from: TitleCoords, to: Option<TitleCoords>) {
+        self.path.insert(came_from, to);
+    }
+    pub fn reconstruct_path(&mut self, start: TitleCoords, goal: TitleCoords) {
+        let mut current = goal;
+
+        while current != start {
+            self.solution.push(current);
+            let c = *self
+                .path
+                .get(&current)
+                .expect("Title for path reconstructing is wrong");
+            current = c.unwrap();
+        }
+
+        self.solution.push(start);
+    }
+}
+
+#[cfg(test)]
+mod unit_test {
+    use super::*;
+    #[test]
+    fn path_finding() {
+        let mut path_finder = Pathfinder::default();
+
+        // [start] -> [1] -> [2] -> [3] -> [goal]
+        let path_start = TitleCoords { x: 0, y: 0 };
+        let path_1 = TitleCoords { x: 1, y: 0 };
+        let path_2 = TitleCoords { x: 2, y: 0 };
+        let path_3 = TitleCoords { x: 3, y: 0 };
+        let path_goal = TitleCoords { x: 3, y: 1 };
+
+        let exp_solution_path = vec![path_goal, path_3, path_2, path_1, path_start];
+
+        path_finder.create_path(path_start, None);
+        path_finder.create_path(path_1, Some(path_start));
+        path_finder.create_path(path_2, Some(path_1));
+        path_finder.create_path(path_3, Some(path_2));
+        path_finder.create_path(path_goal, Some(path_3));
+
+        path_finder.reconstruct_path(path_start, path_goal);
+
+        assert_eq!(exp_solution_path, path_finder.solution);
     }
 }
